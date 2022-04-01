4a4a2ce87dbdc5480f509f4dc9994c3aedc72238
mikehoods <62457440+mikehoods@users.noreply.github.com>
2022-04-01 14:37:04 -0500

    Added hint feature to autofill up to three random letters

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import alphabet from '../utils/alphabet';
 import EncryptedText from '../components/EncryptedText'
 import fisherYatesShuffle from '../utils/fisherYatesShuffle';
+import puzzle_bulb from '../assets/puzzle_bulb.png';
+import puzzle_bulb_clicked from '../assets/puzzle_bulb_clicked.png';
 import useFetch from '../hooks/useFetch';
 
 function Home() {
@@ -11,6 +13,10 @@ function Home() {
     const [name, setName] = useState(null);
     const [quote, setQuote] = useState(null);
     const [shuffledAlphabet, setShuffledAlphabet] = useState(null);
+    const [hints, setHints] = useState([]);
+    const [hintsGiven, setHintsGiven] = useState([false, false, false])
+    const [randLetters, setRandLetters] = useState([])
+
 
     const { data, error, isLoading } = useFetch();
 
@@ -40,9 +46,40 @@ function Home() {
         if (name && quote && shuffledAlphabet) encryptQuote()
     }, [quote, name, shuffledAlphabet])
 
+    useEffect(() => {
+        if (hints.length > 0) {
+            let copyHints = hintsGiven
+            copyHints[hints.length -1] = true
+            setHintsGiven([...copyHints])
+        } 
+    }, [hints])
+
     const handleChange = (e) => {
-        let letters = document.getElementsByClassName(e.target.className)
-        for (let i=0; i < letters.length; i++) letters[i].value = e.target.value  
+        let letter = document.getElementsByClassName(e.target.className)
+        for (let i=0; i < letter.length; i++) letter[i].value = e.target.value  
+    }
+
+    const handleClick = () => {
+        const rand = Math.floor(Math.random() * 26)
+        if (randLetters.includes(rand)) {
+            handleClick()
+        } else {
+            setRandLetters([...randLetters, rand])
+            let letter = document.getElementsByClassName(`char_input ${shuffledAlphabet[rand].toLowerCase()}`)
+            if (letter.length === 0) {
+                letter = document.getElementsByClassName(`char_input ${shuffledAlphabet[rand]}`)
+                if(letter.length === 0) handleClick()    
+            }
+            if (letter.length != 0) setHints([...hints, alphabet[rand]])
+            for (let i=0; i < letter.length; i++) letter[i].value = alphabet[rand]
+            
+        
+            console.log(`Attempted Hints: ${randLetters}`)
+            console.log(`Hints given: ${hints}`)
+            console.log(letter)
+        }
+        
+        console.log(rand, alphabet[rand], shuffledAlphabet[rand])
     }
 
     return (
@@ -50,7 +87,17 @@ function Home() {
             {isLoading && <div>{isLoading}</div>}
             {error && <div>{error}</div>}
             {cryptoQuote && cryptoName && <div>
-                <h1>Crypto Quote</h1>
+                <div className="header">
+                    <h1>Crypto Quote</h1>
+                    <div>{hintsGiven.map((h, i) => {
+                        return (
+                            hintsGiven[i] === true ?
+                                <img key={i} className='used_hint' src={puzzle_bulb_clicked} alt='grey_lightbulb' />
+                                : <img key={i} className='hint' src={puzzle_bulb} alt='lightbulb' onClick={handleClick} />
+                        )
+                        })}
+                    </div>
+                </div>
                 <div className="quote_container">
                     <EncryptedText divName="phrase_div" words={cryptoQuote} onChange={(e) => handleChange(e)} />
                     <EncryptedText divName="author_div" words={cryptoName} onChange={(e) => handleChange(e)} />
